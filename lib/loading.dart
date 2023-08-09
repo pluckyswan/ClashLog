77a6a1acc2f29e7e94b80fca408273993efe6659
Bella <blquintero@miners.utep.edu>
2023-08-08 19:21:54 -0600

    Upload project

--- lib/loading.dart
+++ lib/loading.dart
@@ -9,8 +9,7 @@ late WarLog war;
 /// Stores war information into list.
 List log = [];
 /// Stores custom token to access API.
-const token =
-    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImJlMjk0MDRmLTZlZTctNDczMS05YzZiLTBkNDMwZjgxMmM0OSIsImlhdCI6MTY5MDE0NTk3Mywic3ViIjoiZGV2ZWxvcGVyLzFmMzQ0YWQ2LTE4OWUtZjRhMC00ZGU1LTk4NDliNzdhMTA0YSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjEwNC4yLjE0Mi4yMDAiXSwidHlwZSI6ImNsaWVudCJ9XX0.eL1nLd-z_PPQKECy5PniVD5RwYr1hzJ7rGjBPWpTV7HDbaGeOJ_ttgWDlCtQVRcsdXG2dQIJ4AE6Ph3egP96WQ';
+const token = "API-KEY";
 /// Fetches clan related information.
 fetchClanClass(String tag) async {
   final response = await http.get(
